5bd4d7f36bb5ea148f606d22baab69239779d1a6
vicentgit <vicent@aa.aa>
2023-11-23 19:12:14 +0100

    push

---
+++ EstructuresControl/ej1.py
@@ -0,0 +1,10 @@
+# Obtener numero
+num = int(input("Introduce un numero:"))
+
+#Comparacion del numero
+if num > 0:
+    print ("El numero", num, "es positivo")
+elif num < 0:
+    print ("El numero", num, "es negativo")
+else:
+    print ("El numero", num, "es cero")
